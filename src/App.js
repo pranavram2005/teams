5384fcd0fff144e95c82a487c50e429de2c56886
Deepti <chennaisunday@gmail.com>
2024-03-29 17:21:32 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import S_Card from './components/S_Card';
 
 function App() {
   return (
+    <>
+  <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      TEAM
+      </div> 
+      <div className="container-fluid head">
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
+      <div className="App head">
+      Student Leaders
+      </div> 
+      <div className="container-fluid head">
+      <S_Card/>
+      <S_Card/>
+      </div>
+      <div className="App head">
+      Student Members
+      </div> 
+      <div className="container-fluid head2">
+        <button data-bs-toggle="collapse" data-bs-target="#demo" type="button">Lab</button></div>
+        <div id="demo" className="collapse">
+          <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+        <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        <S_Card/></div>
+        <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        <S_Card/></div> </div>
+        <div className="container-fluid head2">
+        <button data-bs-toggle="collapse" data-bs-target="#demo2" type="button">Human Practices</button></div>
+        <div id="demo2" className="collapse">
+          <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+        <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+          </div>
+          <div className="container-fluid head2">
+          <button data-bs-toggle="collapse" data-bs-target="#demo3" type="button">WebOps</button></div>
+        <div id="demo3" className="collapse">
+          <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+        <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+          </div>
+          <div className="container-fluid head2">
+          <button data-bs-toggle="collapse" data-bs-target="#demo4" type="button">Sponsorships and PR</button></div>
+        <div id="demo4" className="collapse">
+          <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+        <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+          </div>
+          <div className="container-fluid head2">
+          <button data-bs-toggle="collapse" data-bs-target="#demo5" type="button">Media and Design</button></div>
+        <div id="demo5" className="collapse">
+          <div className="head">
+        <S_Card/>
+        <S_Card/>
+        <S_Card/>
+        </div>
+        <div className="head">
+        <S_Card/>
+        <S_Card/>
+        </div>
+          </div>
+      </div></>
   );
 }
 
---
+++ src/components/S_Card.js
@@ -0,0 +1,12 @@
+let S_Card=()=>{
+    return(
+        <div className="card main">
+    <img className="card-img-top image" src="wp4.jpg"/>
+    <div className="card-body">
+    <h4 className="card-title">Name</h4>
+      <p className="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+    </div>
+  </div>
+    );
+}
+export default S_Card
